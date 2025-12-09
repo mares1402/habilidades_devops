8c12afa5ebb7d1bcd609430f05ca1e803004140e
mares1402 <maresd1402@gmail.com>
2025-12-08 21:26:25 -0600

    modificacion del puerto

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.post('/signup', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
